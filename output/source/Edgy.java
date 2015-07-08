52a33a29ba3b55f9e3ce24e265b49395b9db91d9
Yicheng Wang <alex.wyc2098@protonmail.ch>
2015-07-08 13:50:14 -0400

    updated

--- output/source/Edgy.java
+++ output/source/Edgy.java
@@ -1,5 +1,24 @@
-import processing.video.Capture;
-import java.util.Arrays;
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import processing.video.Capture; 
+import java.util.Arrays; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Edgy extends PApplet {
+
+
+
 
 Capture cam;
 float[] deltaX;
@@ -22,13 +41,13 @@ float[] matrixY = {
   1, 2, 1
 };
 
-void setup() {
+public void setup() {
   size(640, 480);
   cam = new Capture(this, 640, 480);
   cam.start();
 }
 
-void draw() {
+public void draw() {
   if (cam.available() != true) {
     return;
   }
@@ -92,7 +111,7 @@ void draw() {
   text("threshold = " + (int)threshold, 20, 50);
 }
 
-float[] getSurroundings(float[] all, int x, int y) {
+public float[] getSurroundings(float[] all, int x, int y) {
   return new float[]{
         all[x - 1 + (y - 1) * width], 
         all[x + (y - 1) * width], 
@@ -106,7 +125,7 @@ float[] getSurroundings(float[] all, int x, int y) {
       };
 }
 
-float dotProduct(float[] vector1, float[] vector2) {
+public float dotProduct(float[] vector1, float[] vector2) {
   float result = 0;
   for (int i = 0 ; i < vector1.length ; i++) {
     result += vector1[i] * vector2[i];
@@ -114,11 +133,20 @@ float dotProduct(float[] vector1, float[] vector2) {
   return result;
 }
 
-void keyTyped() {
+public void keyTyped() {
     if (key=='u') {
         threshold = threshold + 1;
     }
     if (key == 'd') {
         threshold = threshold - 1;
     }
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Edgy" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }

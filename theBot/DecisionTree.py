0209893756c85bdd3179205bb70b558c2eb2854e
Felipe Mansilla <pipe.ny@gmail.com>
2015-07-29 09:34:38 -0400

    something,idk

--- theBot/DecisionTree.py
+++ theBot/DecisionTree.py
@@ -1,30 +1,32 @@
 import nltk
 from sklearn import tree
+from sklearn.externals import joblib
 
-att = open('foo.txt').read()
+att = open('DATA.csv').read()
 
 numLines = 0
 
 l = att.split('\n')
 numLines = len(l)
-attributes = [[]]
+attributes = []
 
 for element in l:
     temp = element.split(',')
     temp2 = []
     for s in temp:
-        temp2.append(int(s))
+        temp2.append(float(s))
     attributes.append(temp2)
 
 samples = []
 x = 0
 while (x < numLines/2):
-    samples[x] = -1
-    x = x + 1
-x = 0
-while (x < numLines):
     samples[x] = 1
+    x = x + 1
+while (x < numLines):
+    samples[x] = -1
     x = x + 1
 
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(attributes,samples)
+
+joblib.dump(clf, "DT/movieReview.dt")

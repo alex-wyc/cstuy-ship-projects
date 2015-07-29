db496ea55c696476db3b7d4e98a54278f5199e76
Yicheng Wang <alex.wyc2098@protonmail.ch>
2015-07-28 20:01:44 -0400

    finalized bayes classifier

--- theBot/bayesClassifier.py
+++ theBot/bayesClassifier.py
@@ -3,42 +3,53 @@
 
 import nltk
 import math
+from sys import argv
 
 review = "review goes here!!!"
 
 posKeywordList = {}
-negKeywordList = {}
 
-def loadData():
-    f = open("PosKeywords.txt", 'r').read().split('\n')[:-1]
+def loadData(mode):
+    if mode == 'U':
+        f = open("Keywords.txt", 'r').read().split('\n')[:-1]
+    elif mode == 'A':
+        f = open("adjKeywords.txt", 'r').read().split('\n')[:-1]
+    elif mode == 'P':
+        f = open("PosKeywords.txt", 'r').read().split('\n')[:-1]
+
+
     for i in f:
         data = i.split()
-        posKeywordList[data[0]] = float(data[1])
+        if mode == 'P':
+            posKeywordList[eval("".join(data[:-1]))] = float(data[-1])
+        else:
+            posKeywordList[data[0]] = float(data[1])
 
-    f = open("NegKeywords.txt", 'r').read().split('\n')[:-1]
-    for i in f:
-        data = i.split()
-        negKeywordList[data[0]] = float(data[1])
+def asciify(text):
+    return "".join([i for i in list(text) if isAlphanumeric(i)])
+
+def isAlphanumeric(char):
+    order = ord(char)
+    return (order >= 48 and order <= 57) or (order >= 65 and order <= 90) or (order >= 97 and order <= 122) or order == 9 or order == 32
 
 def intersection(list1, list2):
     return list(set(list1) & set(list2))
 
 def percentPositive(review):
-    tokens = nltk.pos_tag(nltk.word_tokenize(review))
-    posWordList = intersection((w[0] for w in tokens), posKeywordList.keys())
-    negWordList = intersection((w[0] for w in tokens), negKeywordList.keys())
+    tokens = asciify(review).split()
+    if argv[1] == 'P':
+        tokens = nltk.pos_tag(tokens)
+    posWordList = intersection(tokens, posKeywordList.keys())
     exp = 0
     for k in posWordList:
         exp += math.log(1 - posKeywordList[k]) - math.log(posKeywordList[k])
-    for k in negWordList:
-        exp -= math.log(1 - negKeywordList[k]) - math.log(negKeywordList[k])
     return 1 / (1 + math.exp(exp))
 
 def asciify(text):
     return ''.join([i for i in text if ord(i) < 128])
 
 if __name__ == '__main__':
-    loadData()
+    loadData(argv[1])
     reviewLoc = raw_input("Please enter a route to the review: ")
     review = asciify(open(reviewLoc, 'r').read())
     print "%.2f%% of a great movie." % (percentPositive(review) * 100)
